31ecafe5bb9598bc678c921b7eef13a4cc69c212
SINA <124919260+sinahed@users.noreply.github.com>
2023-09-06 10:04:33 +0200

    Update ft_strcat.c
    fin

--- ex02/ft_strcat.c
+++ ex02/ft_strcat.c
@@ -10,42 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-//#include <unistd.h>
-//#include <string.h>
+#include <stdio.h>
 
-char	*ft_strcat(char *dest, char *src)
+int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
+		if (s1[i] < s2[i])
+		{
+			return (-1);
+		}
+		if (s1[i] > s2[i])
+		{
+			return (1);
+		}
 		i++;
 	}
-	while (src[j] != '0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	return (0);
 }
 
-/*
-int main(void)
-{
-    char destination[15] = "hello ";
-    const char source[] = "world";
 
-    strcat(destination, source);
-
-    printf("Concatenated string: %s\n", destination);
-
-    return 0;
+int main(void) {
+    char s1[] = "abcd";
+    char s2[] = "abcdef";
+    int result = ft_strcmp(s1,s2);
+   printf("%d\n", result);
+    return (0);
 }
-
-*/
